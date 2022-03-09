ae7db8e01afdd37a592f3afcac558ca2d66ea7d2
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-03-09 09:12:46 -0700

    Chapter 3 updated with fixes.

--- Chapter2/src/Chapter3/RectanglePerimeter.java
+++ Chapter2/src/Chapter3/RectanglePerimeter.java
@@ -1,6 +1,6 @@
 /*
 
-Program: VariableInClassExample.java          Last Date of this Revision: February 22, 2022
+Program: VariableInClassExample.java          Last Date of this Revision: March 9, 2022
 
 Purpose: Take three numbers and add them together (context: distances in a race into total)
 
@@ -13,18 +13,29 @@ Course: Computer Science 20
 
 package Chapter3;
 
+import java.util.Scanner;
+
 public class RectanglePerimeter 
 {
 	public static void main(String[] args) 
 	{
-		int l = 13, w = 4;
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.print("Enter a length: ");
+		int l = userInput.nextInt();
+		
+		System.out.print("Enter a width: ");
+		int w = userInput.nextInt();
+		
 		int perimeter = 2 * (l + w);
-		System.out.print(perimeter);
+		System.out.printf("The perimeter of this rectangle is %d units.", perimeter);
 	}
 }
 
 /* Screen dump 
 
-34
+Enter a length: 15
+Enter a width: 8
+The perimeter of this rectangle is 46 units.
 
 */
