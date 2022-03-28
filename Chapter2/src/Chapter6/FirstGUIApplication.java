16250d2f4b13bed222121b9e5a617956d1ef926a
105297008 <105297008@L815WD220002.edu.cbe.ab.ca>
2022-03-28 10:19:38 -0600

    More progress on Prime GUI.

--- Chapter2/src/Chapter6/FirstGUIApplication.java
+++ Chapter2/src/Chapter6/FirstGUIApplication.java
@@ -1,24 +1,14 @@
 package Chapter6;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.*;
 
 public class FirstGUIApplication
-{
-	public void selectPanel()
-	{
-		// the panel for single prime numbers
-		JPanel chooseFunc = new JPanel();
-		chooseFunc.setLayout(new BoxLayout(chooseFunc, BoxLayout.X_AXIS));
-		
-		JCheckBox single = new JCheckBox();
-		JCheckBox range = new JCheckBox();
-		
-		
-	}
-	
+{	
 	public FirstGUIApplication()
 	{
 		JFrame window = new JFrame("Prime Finder");
@@ -27,24 +17,51 @@ public class FirstGUIApplication
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		
+		
 		// title element
 		JLabel title = new JLabel("Prime Finder");
 		title.setFont(new Font("Comic Sans MS", Font.BOLD, 72));
 		title.setHorizontalAlignment(JLabel.CENTER);
 		
+		
 		// prime finder type select
-		JButton button1 = new JButton("Click to not be bored...");
-		button1.setFont(new Font("Comic Sans MS", Font.BOLD, 36));
+		JPanel chooseFunc = new JPanel();
+		chooseFunc.setLayout(new BoxLayout(chooseFunc, BoxLayout.X_AXIS));
+		chooseFunc.setBackground(Color.gray);
+		
+		String[] choices = {"Single", "Range"};
+		JList options = new JList(choices);
+		options.setSelectedIndex(0);
+		options.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
+		
+		Dimension minSize = new Dimension(25, 100);
+		Dimension prefSize = new Dimension(100, 100);
+		Dimension maxSize = new Dimension(Short.MAX_VALUE, 100);
+		
+		chooseFunc.add(new Box.Filler(minSize, prefSize, maxSize));
+		chooseFunc.add(options);
+		chooseFunc.add(new Box.Filler(minSize, prefSize, maxSize));
+
 		
 		// the actual prime finder/checker
-		JButton button2 = new JButton("Main things happen here");
-		button2.setFont(new Font("Comic Sans MS", Font.BOLD, 36));
+		
+		JPanel singlePrimeFunc = new JPanel();
+		singlePrimeFunc.setLayout(new BoxLayout(singlePrimeFunc, BoxLayout.Y_AXIS));
+		singlePrimeFunc.setBackground(Color.lightGray);
+		
+		JLabel description = new JLabel("Enter any number to check for prime:");
+		description.setFont(new Font("Comic Sans MS", Font.BOLD, 36));
+		
+		singlePrimeFunc.add(description);
+//		JButton button2 = new JButton("Main things happen here");
+//		button2.setFont(new Font("Comic Sans MS", Font.BOLD, 36));
+		
 		
 		// add all elements into program layout
 		window.add(panel);
 		panel.add(title, BorderLayout.NORTH);
-		panel.add(button1, BorderLayout.EAST);
-		panel.add(button2, BorderLayout.CENTER);
+		panel.add(chooseFunc, BorderLayout.EAST);
+		panel.add(singlePrimeFunc, BorderLayout.CENTER);
 		
 		window.setSize(1280, 720);
 		window.setVisible(true);
