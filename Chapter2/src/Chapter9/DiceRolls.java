d29072e7bcbc7b306a7b931223c24d81e48e93d6
105297008 <105297008@L815WD220002.edu.cbe.ab.ca>
2022-05-02 10:19:15 -0600

    Working on DiceRolls GUI.

--- Chapter2/src/Chapter9/DiceRolls.java
+++ Chapter2/src/Chapter9/DiceRolls.java
@@ -1,6 +1,6 @@
 /*
 
-Program: DiceRolls.java          Last Date of this Revision: April 28, 2022
+Program: DiceRolls.java          Last Date of this Revision: May 2, 2022
 
 Purpose: List student names in an array and print them.
 
@@ -13,15 +13,41 @@ Course: Computer Programming 20
 
 package Chapter9;
 
-import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class DiceRolls
+public class DiceRolls implements ActionListener
 {
 
 	private final int[] outcomes = new int[19];
-	private final Scanner input = new Scanner(System.in);
 	private int numRolls;
 	private int outcome;
+	
+	private JLabel title;
+	private JLabel output;
+	private JPanel main;
+	private JFrame window;
+	private JTextField input;
+	
+	public DiceRolls()
+	{
+		numRolls = 0;
+		outcome = 0;
+		
+		
+		title = new JLabel("Roll the Dice!");
+		title.setHorizontalAlignment(JLabel.CENTER);
+		output = new JLabel("test");
+		output.setHorizontalAlignment(JLabel.CENTER);
+		input = new JTextField();
+		
+		window = new JFrame("Dice Rolls Application");
+		main = new JPanel(new BorderLayout());
+		
+		main.add(title, BorderLayout.NORTH);
+		main.add(input, BorderLayout.CENTER);
+	}
 	
 	public void setRolls(int rolls)
 	{
@@ -42,21 +68,44 @@ public class DiceRolls
 		}
 	}
 	
+	public void launch()
+	{
+		window.add(main);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(500, 500);
+		window.setVisible(true);
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		System.out.println("Hello World!");
+	}
+	
+	
+	
 	// test main
 	public static void main(String[] args)
 	{
-		DiceRolls console = new DiceRolls();
-		console.setRolls(1000);
-		console.rollDice();
-		
-		for (int i = 3; i < console.outcomes.length; i++)
-		{
-			System.out.println((i) + ": " + console.outcomes[i]);
-		}
-		
-		System.out.println("Number of rolls: " + console.getRolls());
-		System.out.println("Array length: " + console.outcomes.length);
+//		DiceRolls console = new DiceRolls();
+//		console.setRolls(1000);
+//		console.rollDice();
+//		
+//		for (int i = 3; i < console.outcomes.length; i++)
+//		{
+//			System.out.println((i) + ": " + console.outcomes[i]);
+//		}
+//		
+//		System.out.println("Number of rolls: " + console.getRolls());
+//		System.out.println("Array length: " + console.outcomes.length);
+	
+		DiceRolls app = new DiceRolls();
+		app.launch();
+	
 	}
+
+	
 
 }
 
