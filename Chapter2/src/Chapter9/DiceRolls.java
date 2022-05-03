61e8f16733e860931e5817c6651b3444199c38bf
105297008 <105297008@L815WD220002.edu.cbe.ab.ca>
2022-05-03 11:57:44 -0600

    DiceRolls GUI work continued, app is functional and GUI works.

--- Chapter2/src/Chapter9/DiceRolls.java
+++ Chapter2/src/Chapter9/DiceRolls.java
@@ -21,45 +21,84 @@ public class DiceRolls implements ActionListener
 {
 
 	private final int[] outcomes = new int[19];
-	private int numRolls;
-	private int outcome;
+	private final Object[] labels = new Object[16];
+	private int numRolls, outcome;
+	private String print;
 	
-	private JLabel title;
-	private JLabel output;
 	private JPanel main;
+	private JPanel center;
 	private JFrame window;
 	private JTextField input;
 	
+	private JLabel title;
+	private JLabel output;
+	private JLabel instruction;
+	
+	private JLabel three;
+	private JLabel four;
+	private JLabel five;
+	private JLabel six; 
+	private JLabel seven;
+	private JLabel eight;
+	private JLabel nine;
+	private JLabel ten;
+	private JLabel eleven;
+	private JLabel twelve;
+	private JLabel thirteen;
+	private JLabel fourteen;
+	private JLabel fifteen;
+	private JLabel sixteen;
+	private JLabel seventeen;
+	private JLabel eighteen;
+	
 	public DiceRolls()
 	{
-		numRolls = 0;
-		outcome = 0;
+		numRolls = 0; outcome = 0;
+		print = "";
+		
+		labels[0] = three;
+		labels[1] = four;
+		labels[2] = five;
+		labels[3] = six;
+		labels[4] = seven;
+		labels[5] = eight;
+		labels[6] = nine;
+		labels[7] = ten;
+		labels[8] = eleven;
+		labels[9] = twelve;
+		labels[10] = thirteen;
+		labels[11] = fourteen;
+		labels[12] = fifteen;
+		labels[13] = sixteen;
+		labels[14] = seventeen;
+		labels[15] = eighteen;
 		
 		
 		title = new JLabel("Roll the Dice!");
 		title.setHorizontalAlignment(JLabel.CENTER);
+		instruction = new JLabel("Write the number of dice rolls, then press ENTER to roll: ");
 		output = new JLabel("test");
 		output.setHorizontalAlignment(JLabel.CENTER);
-		input = new JTextField();
+		input = new JTextField(10);
+		input.addActionListener(this);
 		
 		window = new JFrame("Dice Rolls Application");
 		main = new JPanel(new BorderLayout());
+		center = new JPanel(new FlowLayout());
 		
 		main.add(title, BorderLayout.NORTH);
-		main.add(input, BorderLayout.CENTER);
+		main.add(center, BorderLayout.CENTER);
+		main.add(output, BorderLayout.SOUTH);
+		center.add(instruction);
+		center.add(input);
 	}
 	
-	public void setRolls(int rolls)
+	private void setRolls(int rolls)
 	{
 		numRolls = rolls;
 	}
 	
-	public int getRolls()
-	{
-		return numRolls;
-	}
-	
-	public void rollDice()
+	private void rollDice()
 	{
 		for (int r = 0; r < numRolls; r++)
 		{
@@ -68,21 +107,33 @@ public class DiceRolls implements ActionListener
 		}
 	}
 	
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+//		System.out.println("Hello World!");
+		
+		setRolls(Integer.parseInt(input.getText()));
+		input.setText(null);
+		
+		rollDice();
+		
+		for (int i = 3; i < outcomes.length; i++)
+		{
+			print += String.format("Rolled %d: %d; ", i, outcomes[i]);
+		}
+		
+		output.setText(print);
+		
+	}
+	
+	
 	public void launch()
 	{
 		window.add(main);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(500, 500);
+		window.setSize(500, 200);
 		window.setVisible(true);
 	}
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		System.out.println("Hello World!");
-	}
-	
 	
 	
 	// test main
