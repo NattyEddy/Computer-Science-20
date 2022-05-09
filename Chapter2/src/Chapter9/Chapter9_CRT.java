fbfa3525dc7bc0b77983a9e5791d2ba863d555c5
105297008 <105297008@L815WD220002.edu.cbe.ab.ca>
2022-05-09 10:18:29 -0600

    Chapter 9 CRT complete.

---
+++ Chapter2/src/Chapter9/Chapter9_CRT.java
@@ -0,0 +1,7 @@
+// 1.	index: 2
+// 2.	int[] quantities = new int[20];
+// 3.	double[] heights = {1.65, 2.15, 4.95};
+// 4.	for (int i : grades)
+//		{
+//			System.out.println(i);
+//		}
--- Chapter2/src/Chapter9/StudentRoster.java
+++ Chapter2/src/Chapter9/StudentRoster.java
@@ -1,6 +1,6 @@
 /*
 
-Program: StudentRoster.java          Last Date of this Revision: April 26, 2022
+Program: StudentRoster.java          Last Date of this Revision: May 9, 2022
 
 Purpose: List student names in an array and print them.
 
@@ -40,9 +40,9 @@ public class StudentRoster
 		System.out.println("\n=============================\n");
 		System.out.println("STUDENT ROSTER:");
 
-		for (int i = 0; i < studentNames.length; i++)
+		for (String i : studentNames)
 		{
-			System.out.println(studentNames[i]);
+			System.out.println(i);
 		}
 
 	}
