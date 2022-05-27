c5e1c46ff86c738ccf1a9e0877ad7ec12dad3fb1
105297008 <105297008@L815WD220002.edu.cbe.ab.ca>
2022-05-27 09:41:45 -0600

    Thermometer complete.

---
+++ Chapter2/src/Phidgets/Thermometer.java
@@ -0,0 +1,107 @@
+/*
+
+Program: Thermometer.java          Last Date of this Revision: May 27, 2022
+
+Purpose: Simulate a thermometer with the Phidgets box.
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
+
+package Phidgets;
+
+// Add Phidgets Library
+import com.phidget22.*;
+
+public class Thermometer
+{
+    public static void main(String[] args) throws Exception
+    {
+
+        // Create | Components
+        TemperatureSensor temperatureSensor = new TemperatureSensor();
+        DigitalInput decreaseButton = new DigitalInput();
+        DigitalInput increaseButton = new DigitalInput();
+        DigitalOutput redLED = new DigitalOutput();
+        DigitalOutput greenLED = new DigitalOutput();
+        
+        // Create | Variables
+        
+        double celsius;
+        double fahrenheit;
+        int setTemp = 22;
+        int change = 0;
+        boolean decreaseButtonState = false;
+        boolean increaseButtonState = false;
+        
+        //Address 
+        decreaseButton.setHubPort(0);
+        decreaseButton.setIsHubPortDevice(true);
+        
+        increaseButton.setHubPort(5);
+        increaseButton.setIsHubPortDevice(true);
+        
+        redLED.setHubPort(1);
+        redLED.setIsHubPortDevice(true);
+        
+        greenLED.setHubPort(4);
+        greenLED.setIsHubPortDevice(true);
+        
+        // Open 
+        temperatureSensor.open(1000);
+        decreaseButton.open(1000);
+        increaseButton.open(1000);
+        redLED.open(1000);
+        greenLED.open(1000);
+
+        // Use your Phidgets
+        while (true)
+        {
+            
+        	celsius = temperatureSensor.getTemperature();
+        	fahrenheit = celsius * 1.8 + 32;
+        	
+        	// detect button presses and change set temperature
+        	if (decreaseButtonState != decreaseButton.getState())
+            {
+        		setTemp -= decreaseButton.getState() ? 1 : 0;
+        		change += decreaseButton.getState() ? 1 : 0;
+            	decreaseButtonState = decreaseButton.getState();
+            }
+        	if (increaseButtonState != increaseButton.getState())
+            {
+        		setTemp += increaseButton.getState() ? 1 : 0;
+        		change += increaseButton.getState() ? 1 : 0;
+            	increaseButtonState = increaseButton.getState();
+            }
+        	
+        	// detects change in temperature to print new temp
+        	if (change != 0)
+        	{
+        		System.out.printf("Set Temp: %d°C\n", setTemp);
+        		change = 0;
+        	}
+        	
+        	
+        	// changes light when in range
+        	if (celsius >= setTemp - 2 && celsius <= setTemp + 2)
+        	{
+        		redLED.setState(false);
+        		greenLED.setState(true);
+        	}
+        	else
+        	{
+        		redLED.setState(true);
+        		greenLED.setState(false);
+        	}
+  
+        	Thread.sleep(10);
+            
+        }
+    }
+}
+  
